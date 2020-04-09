b13230cec95ae9485c0e11bbf426bb53c1c8174c
Smith Orakpo <grapevinecode@gmail.com>
2020-04-09 00:47:33 -0700

    scaffold router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import "dotenv/config"
 import express from "express"
-
+import router from "../router"
 import middleware from "../middleware"
 
 const app = express()
 middleware(app)
+app.use("/", router)
 
-app.get("/", (request, response) =>
-	response.status(200).send("coffee house api")
-)
+app.use((request, response) => response.status(404).send("resource not found"))
+app.use((error, request, response, next) => {
+	console.error(error.message)
+	response.status(500).send("internal server error")
+})
 
 app.listen(process.env.PORT)
